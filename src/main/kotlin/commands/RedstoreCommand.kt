dd0b28df17e9dd4613f915147a5ed755298df1cc
Martin Dørum <martid0311@gmail.com>
2024-05-10 14:26:57 +0200

    improve interface

--- src/main/kotlin/commands/RedstoreCommand.kt
+++ src/main/kotlin/commands/RedstoreCommand.kt
@@ -14,20 +14,119 @@ import java.io.File
 class RedstoreCommand(private val redstore: RedstORE): BaseCommand() {
     @Default
     @CatchUnknown
+    @Subcommand("help")
+    @CommandCompletion("connect|disconnect|version|help")
+    fun help(p: Player, @Optional command: String?) {
+        if (command == null || command == "all") {
+            p.sendMessage("Available RedstORE commands:");
+            p.sendMessage("/redstore connect <read|write> <file> <params...>");
+            p.sendMessage("   Create a new RedstORE connection at your location.");
+            p.sendMessage("   Use '/redstore help connect' for info on params.");
+            p.sendMessage("/redstore disconnect");
+            p.sendMessage("   Disconnect the RedstORE connection at your location.");
+            p.sendMessage("/redstore version");
+            p.sendMessage("   Print version string.");
+            p.sendMessage("/redstore help <subcommand>");
+            p.sendMessage("   Print this help text, or help with a subcommand.");
+        } else if (command == "connect") {
+            p.sendMessage("/redstore connect <read|write> <file> <params...>");
+            p.sendMessage("   Create a new RedstORE connection at your location.");
+            p.sendMessage("   Available parameters:");
+            p.sendMessage("   dir=<direction>: Set direction. One of:");
+            p.sendMessage("       north, south, east, west, up, down");
+            p.sendMessage("       Default: determined by camera direction");
+            p.sendMessage("   addr=<N>: Set the number of address bits.");
+            p.sendMessage("       Default: 4");
+            p.sendMessage("   ws=<N>: Set the number of bits in a word.");
+            p.sendMessage("       Default: 8");
+            p.sendMessage("   ps=<N>: Set the number of words in a page.");
+            p.sendMessage("       Default: 8");
+        } else if (command == "disconnect") {
+            p.sendMessage("/redstore disconnect");
+            p.sendMessage("   Disconnect the RedstORE connection at your location.");
+        } else if (command == "version") {
+            p.sendMessage("/redstore version");
+            p.sendMessage("   Print version string.");
+        } else if (command == "help") {
+            p.sendMessage("/redstore help <subcommand>");
+            p.sendMessage("   Print general help text, or help with a subcommand.");
+        } else {
+            p.sendMessage("Unknown subcommand: ${command}.");
+            p.sendMessage("Use '/redstone help' for help.");
+        }
+    }
+
     @Subcommand("version")
     fun info(player: Player) {
         player.sendMessage("Hi! I'm RedstORE ${redstore.description.version}");
     }
 
     @Subcommand("connect")
-    @CommandCompletion("read|write")
-    fun connect(player: Player, modeStr: String) {
-        val mode = when (modeStr) {
+    @CommandCompletion("read|write <file> <params...>")
+    fun connect(
+        player: Player,
+        mode: String,
+        path: String,
+        params: Array<String>,
+    ) {
+        @Suppress("NAME_SHADOWING")
+        val mode = when (mode) {
             "read" -> ConnMode.READ;
             "write" -> ConnMode.WRITE;
             else -> {
                 player.sendMessage(
-                    "Unknown mode: '${modeStr}'. Expected 'read' or 'write'.");
+                    "Unknown mode: '${mode}'. Expected 'read' or 'write'.");
+                return;
+            }
+        }
+
+        var direction = player.getFacing();
+        var addressBits = 4;
+        var wordSize = 8;
+        var pageSize = 8;
+
+        for (param in params) {
+            val parts = param.split("=", limit=2);
+            if (parts.size != 2) {
+                player.sendMessage("Parameter '${param}' is missing an '='");
+                return;
+            }
+
+            val k = parts[0];
+            val v = parts[1];
+            if (k == "dir") {
+                direction = when (v) {
+                    "north" -> BlockFace.NORTH;
+                    "south" -> BlockFace.SOUTH;
+                    "east" -> BlockFace.EAST;
+                    "west" -> BlockFace.WEST;
+                    "up" -> BlockFace.UP;
+                    "down" -> BlockFace.DOWN;
+                    else -> {
+                        player.sendMessage("Unknown direction: ${v}");
+                        return@connect;
+                    }
+                }
+            } else if (k == "addr") {
+                addressBits = v.toInt();
+                if (addressBits < 1 || addressBits > 16) {
+                    player.sendMessage("Invalid address size: ${addressBits}");
+                    return;
+                }
+            } else if (k == "ws") {
+                wordSize = v.toInt();
+                if (wordSize < 1 || wordSize > 32) {
+                    player.sendMessage("Invalid word size: ${wordSize}");
+                    return;
+                }
+            } else if (k == "ps") {
+                pageSize = v.toInt();
+                if (pageSize < 1 || pageSize > 512) {
+                    player.sendMessage("Invalid page size: ${pageSize}");
+                    return;
+                }
+            } else {
+                player.sendMessage("Unknown parameter: '${k}'");
                 return;
             }
         }
@@ -36,12 +135,12 @@ class RedstoreCommand(private val redstore: RedstORE): BaseCommand() {
         redstore.addStoreConnection(player, ConnectionProperties(
             mode = mode,
             origin = block,
-            direction = player.getFacing(),
-            addressBits = 4,
-            wordSize = 16,
-            pageSize = 8,
+            direction = direction,
+            addressBits = addressBits,
+            wordSize = wordSize,
+            pageSize = pageSize,
 
-            file = File("hello.txt"),
+            file = File(path),
         ));
     }
 
